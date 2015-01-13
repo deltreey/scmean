7e68066cbabce74a4e35d33470cd59783d4add4c
deltreey <suicidolt@gmail.com>
2015-01-12 23:46:48 -0500

    repository implementation supports more commands but seems to require webdav or cgi

--- server/api/repository/index.js
+++ server/api/repository/index.js
@@ -7,12 +7,17 @@ var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
+router.get('/:id/HEAD', controller.showHead);
+router.get('/:id/info/refs', controller.refs);
+router.get('/:id//objects/info/:objectType', controller.objects);
+router.get('/:id/objects/pack/:pack', controller.getPack);
+router.get('/:id/objects/:group/:object', controller.looseObject);
 router.post('/', controller.create);
+router.post('/:id/git-upload-pack', controller.uploadPack);
+router.post('/:id/git-receive-pack', controller.recievePack);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
 router.delete('/:id', controller.destroy);
-router.get('/:id/:branch', controller.showBranch);
-router.post('/:id/git-receive/pack', controller.recievePack);
-router.get('/:id/info/refs', controller.refs);
+//router.propfind('/:id', controller.propfind);
 
 module.exports = router;
--- server/api/repository/repository.controller.js
+++ server/api/repository/repository.controller.js
@@ -9,22 +9,35 @@ var config = require('../../config/environment');
 
 exports.refs = function(req, res) {
   if (req.param('service') &&
-    req.param('service') === 'git-receive-pack')
+    (
+      req.param('service') === 'git-receive-pack' ||
+      req.param('service') === 'git-upload-pack'
+    ))
   {
     res.setHeader('Expires', 'Fri, 01 Jan 1980 00:00:00 GMT');
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Cache-Control', 'no-cache, max-age=0, must-revalidate');
-    res.setHeader('Content-Type', 'application/x-git-receive-pack-advertisement');
+    res.setHeader('Content-Type', 'application/x-' + req.param('service') + '-advertisement');
+    var packet = '# service=' + req.param('service') + '\n';
+    var length = packet.length + 4;
+    var hex = '0123456789abcdef';
+    var prefix = hex.charAt(length >> 12) & 0xf;
+    prefix = prefix + hex.charAt(length >> 8) & 0xf;
+    prefix = prefix + hex.charAt(length >> 4) & 0xf;
+    prefix = prefix + hex.charAt(length) & 0xf;
+    var result = prefix + packet + '0000';
+
     var gitDir = config.root + config.git.directory;
     var repoDir = gitDir + '/' + req.params.id + '.git';
-    var command = 'git-receive-pack --stateless-rpc --advertise-refs ' + repoDir;
+    var command = req.param('service') + ' --stateless-rpc --advertise-refs ' + repoDir;
     exec(command, {
       cwd: repoDir 
     }, function(error, stdout, stderr) {
       if (error !== null) {
         return handleError(res, 'exec error: ' + error);
       }
-      console.log(stdout);
+      result += stdout;
+      console.log(result);
       return res.json(200, stdout);
     });
   }
@@ -34,34 +47,109 @@ exports.refs = function(req, res) {
 }
 
 exports.recievePack = function(req, res) {
-  if (req.param('service') &&
-    req.param('service') === 'git-receive-pack')
-  {
-    res.setHeader('Expires', 'Fri, 01 Jan 1980 00:00:00 GMT');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Cache-Control', 'no-cache, max-age=0, must-revalidate');
-    res.setHeader('Content-Type', 'application/x-git-receive-pack-result');
-    var gitDir = config.root + config.git.directory;
-    var repoDir = gitDir + '/' + req.params.id + '.git';
-    var command = 'git-receive-pack --stateless-rpc ' + repoDir;
-    exec(command, {
-      cwd: repoDir 
-    }, function(error, stdout, stderr) {
-      if (error !== null) {
-        return handleError(res, 'exec error: ' + error);
-      }
-      console.log(stdout);
-      return res.json(200, stdout);
-    });
-  }
-  else {
-    return res.send(404);
-  }
+  res.setHeader('Expires', 'Fri, 01 Jan 1980 00:00:00 GMT');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Cache-Control', 'no-cache, max-age=0, must-revalidate');
+  res.setHeader('Content-Type', 'application/x-git-receive-pack-result');
+  var gitDir = config.root + config.git.directory;
+  var repoDir = gitDir + '/' + req.params.id + '.git';
+  var command = 'git-receive-pack --stateless-rpc ' + repoDir;
+  exec(command, {
+    cwd: repoDir 
+  }, function(error, stdout, stderr) {
+    if (error !== null) {
+      return handleError(res, 'exec error: ' + error);
+    }
+    console.log(stdout);
+    return res.json(200, stdout);
+  });
 }
 
-exports.showBranch = function (req, res) {
-  return res.json(404);
+exports.uploadPack = function(req, res) {
+  res.setHeader('Expires', 'Fri, 01 Jan 1980 00:00:00 GMT');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Cache-Control', 'no-cache, max-age=0, must-revalidate');
+  res.setHeader('Content-Type', 'application/x-git-upload-pack-result');
+  var gitDir = config.root + config.git.directory;
+  var repoDir = gitDir + '/' + req.params.id + '.git';
+  var command = 'git-upload-pack --stateless-rpc ' + repoDir;
+  exec(command, {
+    cwd: repoDir 
+  }, function(error, stdout, stderr) {
+    if (error !== null) {
+      return handleError(res, 'exec error: ' + error);
+    }
+    console.log(stdout);
+    return res.json(200, stdout);
+  });
+}
+
+exports.showHead = function (req, res) {
+  getFile('HEAD', res, req.params.id, 'text/plain');
 };
+
+exports.objects = function (req, res) {
+  var contentType = 'text/plain';
+  if (req.params.objectType === 'packs') {
+    contentType = 'application/x-git-packed-objects';
+  }
+  getFile('objects/info/' + req.params.objectType, res, req.params.id, contentType);
+};
+
+exports.getPack = function (req, res) {
+  var contentType = 'application/x-git-packed-objects';
+  if (req.params.pack.indexOf('.idx') === req.params.pack.length - 5) {
+    contentType = 'application/x-git-packed-objects-toc';
+  }
+  getFile('objects/pack/' + req.params.pack, res, req.params.id, contentType);
+};
+
+exports.looseObject = function (req, res) {
+  getFile('objects/' + req.params.group + '/' + req.params.object, res, req.params.id, 'application/x-git-loose-object');
+};
+
+function getFile(fileLoc, res, gitId, contentType) {
+  res.setHeader('Expires', 'Fri, 01 Jan 1980 00:00:00 GMT');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Cache-Control', 'no-cache, max-age=0, must-revalidate');
+  res.setHeader('Content-Type', contentType);
+
+  var gitDir = config.root + config.git.directory;
+  var repoDir = gitDir + '/' + gitId + '.git';
+  var filename = repoDir + '/' + fileLoc;
+  fs.readFile(filename, 'utf8', function(err, data) {
+    if (err) { return handleError(res, err); }
+    return res.send(200, data);
+  });
+}
+
+//exports.propfind = function (req, res) {
+//  res.setHeader('Expires', 'Fri, 01 Jan 1980 00:00:00 GMT');
+//  res.setHeader('Pragma', 'no-cache');
+//  res.setHeader('Cache-Control', 'no-cache, max-age=0, must-revalidate');
+//  res.setHeader('Content-Type', 'application/x-' + 'git-receive-pack' + '-advertisement');
+//  var packet = '# service=' + 'git-receive-pack' + '\n';
+//  var length = packet.length + 4;
+//  var hex = '0123456789abcdef';
+//  var prefix = hex.charAt(length >> 12) & 0xf;
+//  prefix = prefix + hex.charAt(length >> 8) & 0xf;
+//  prefix = prefix + hex.charAt(length >> 4) & 0xf;
+//  prefix = prefix + hex.charAt(length) & 0xf;
+//  var result = prefix + packet + '0000';
+//
+//  var gitDir = config.root + config.git.directory;
+//  var repoDir = gitDir + '/' + req.params.id + '.git';
+//  var command = 'git-receive-pack' + ' --stateless-rpc --advertise-refs ' + repoDir;
+//  exec(command, {
+//    cwd: repoDir 
+//  }, function(error, stdout, stderr) {
+//    if (error !== null) {
+//      return handleError(res, 'exec error: ' + error);
+//    }
+//    result += stdout;
+//    return res.json(200, result);
+//  });
+//};
 
 // Get list of repositories
 exports.index = function(req, res) {
