349c2fac8cf7aeac88fee5a8a0264bdf63aed1e8
deltreey <suicidolt@gmail.com>
2015-03-23 12:45:37 -0400

    fixing tests

--- server/api/build/build.spec.js
+++ server/api/build/build.spec.js
@@ -4,11 +4,11 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('GET /api/builds', function() {
+describe('GET /api/v1/builds', function() {
 
   it('should respond with JSON array', function(done) {
     request(app)
-      .get('/api/builds')
+      .get('/api/v1/builds')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
