40476d96410c24948626ae81aa2268efd481454d
deltreey <suicidolt@gmail.com>
2015-06-08 22:19:14 -0400

    wait for request to complete before updating UI
    also, tab widths

--- client/app/repositories/repositories.controller.js
+++ client/app/repositories/repositories.controller.js
@@ -5,38 +5,42 @@ angular.module('scmeanApp')
     $scope.repositories = [];
 
     function UpdateRepositoryList() {
-        $http.get('/api/v1/repositories')
-            .success(function(data) {
-                $scope.repositories = [];
-                for (var d = 0; d < data.length; ++d) {
-                    $scope.repositories.push({
-                        Name: data[d].name,
-                        Url: {
-                            Clone: data[d].url
-                        }
-                    });
-                }
-            })
-            .error(function(data) {
-                console.log('Error retrieving repositories.');
-                console.error(data);
+      $http.get('/api/v1/repositories')
+        .success(function (data) {
+          $scope.repositories = [];
+          for (var d = 0; d < data.length; ++d) {
+            $scope.repositories.push({
+              Name: data[d].name,
+              Url: {
+                Clone: data[d].url
+              }
             });
+          }
+        })
+        .error(function (data) {
+          console.log('Error retrieving repositories.');
+          console.error(data);
+        });
     }
 
     $scope.createRepoDialog = function() {
-        var createModal = Modal.input.createRepository(function(result) {
-            console.log('Creating Repository: ' + result);
-            $http.post('/api/v1/repositories', { name: result });
+      var createModal = Modal.input.createRepository(function(result) {
+        console.log('Creating Repository: ' + result);
+        $http.post('/api/v1/repositories', { name: result })
+          .success(function () {
             new UpdateRepositoryList();
-        });
-        createModal();
+          });
+      });
+      createModal();
     };
     $scope.deleteRepo = function(index) {
-        var deleteModal = Modal.confirm.delete(function () {
-            $http.delete('/api/v1/repositories/' + $scope.repositories[index].Name);
+      var deleteModal = Modal.confirm.delete(function () {
+        $http.delete('/api/v1/repositories/' + $scope.repositories[index].Name)
+          .success(function () {
             new UpdateRepositoryList();
-        });
-        deleteModal($scope.repositories[index].Name);
+          });
+      });
+      deleteModal($scope.repositories[index].Name);
     };
 
     new UpdateRepositoryList();
